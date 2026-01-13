4f668ddcfa8089933db6b357fb64f2ba36f9bc49
thon18valentim <othonvvalentim@gmail.com>
2026-01-12 22:07:48 -0300

    UseCreatedEventConsumer logging

--- src/FCG.Notifications.Services/Consumers/UseCreatedEventConsumer.cs
+++ src/FCG.Notifications.Services/Consumers/UseCreatedEventConsumer.cs
@@ -8,7 +8,9 @@ namespace FCG.Notifications.Services.Consumers
 	{
 		public Task Consume(ConsumeContext<UserCreatedEvent> context)
 		{
-			logger.LogInformation("User Created Event: E-mail sent successfully");
+			logger.LogInformation("E-mail de boas vindas enviado o usuário #{} com e-mail {}", 
+				context.Message.UserId, context.Message.Email);
+
 			return Task.CompletedTask;
 		}
 	}
